0c5481af002af464cd57aac3ffbfed9cf462786f
Shivani Palya <shivanipalya55@gmail.com>
2025-03-05 23:24:50 +0530

    feat: Add data

---
+++ src/config/Footer/index.tsx
@@ -0,0 +1,14 @@
+export const FooterData = [
+    {
+      text: "Work",
+      link: "/work",
+    },
+    {
+      text: "About",
+      link: "/about",
+    },
+    {
+      text: "Let's talk",
+      link: "/contact",
+    },
+  ];
---
+++ src/config/NavBar/index.tsx
@@ -0,0 +1,22 @@
+export const NavbarData = [
+  {
+    text: "Work",
+    link: "/work",
+  },
+  {
+    text: "About",
+    link: "/about",
+  },
+  {
+    text: (
+      <>
+        Let&apos;s talk<sup> +</sup>
+      </>
+    ),
+    link: "/contact",
+  },
+  {
+    text: "Exp",
+    link: "/experience",
+  },
+];
